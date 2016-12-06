5dbcebbc983758ba425006f1051ca095c62a24a5
Richard Chatterton <richard.chatterton@contegix.com>
2016-12-06 14:54:11 -0600

    Also allow myid to be set from an environment variable

--- zk-init.sh
+++ zk-init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ZK=$1
-MYID=1
 
 cd /tmp/zookeeper
 if [ -n "$ZK" ] 
@@ -20,8 +19,6 @@ then
       echo "$el"
     done | sort -n) )
   
-  # get the next increasing number from the sequence
-  MYID=$((${sorted_id_list[${#sorted_id_list[@]}-1]}+1))
   echo $output >> /tmp/zookeeper/conf/zoo.cfg.dynamic
   echo "server.$MYID=$IPADDRESS:2888:3888:observer;2181" >> /tmp/zookeeper/conf/zoo.cfg.dynamic
   cp /tmp/zookeeper/conf/zoo.cfg.dynamic /tmp/zookeeper/conf/zoo.cfg.dynamic.org
